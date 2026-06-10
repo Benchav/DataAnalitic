64734199704ef1ef5bd76f46f998b9ce15f3728c
Benchav <joshua44benja@gmail.com>
2026-06-10 13:43:59 -0600

    feat: conection db

--- Operations/Conexiones/BDConnection.cs
+++ Operations/Conexiones/BDConnection.cs
@@ -8,15 +8,15 @@ namespace BusinessLogic.Connection
 		public WDataMapper? BDDestino { get; set; }
 		public BDConnection()
 		{
-			BDOrigen = SqlADOConexion.BuildDataMapper("JB\\SQLEXPRESS", "sa", "123", "BDOrigen");
-			BDDestino = SqlADOConexion.BuildDataMapper("JB\\SQLEXPRESS", "sa", "123", "BDDestino");
+			BDOrigen = SqlADOConexion.BuildDataMapper("JB\\SQLEXPRESS", "sa", "123", "BDOrigenBiblioteca");
+			BDDestino = SqlADOConexion.BuildDataMapper("JB\\SQLEXPRESS", "sa", "123", "DW_Biblioteca_MINED");
 			BDDestino?.GDatos.TestConnection();
 			BDOrigen?.GDatos.TestConnection();
 		}
 
 		public bool InitMainConnection(bool isDebug = false)
 		{
-			return SqlADOConexion.IniciarConexion("sa", "123", "JB\\SQLEXPRESS", "DW_Bienestar_Psicoemocional");
+			return SqlADOConexion.IniciarConexion("sa", "123", "JB\\SQLEXPRESS", "DW_Biblioteca_MINED");
 		}
 	}
 }
